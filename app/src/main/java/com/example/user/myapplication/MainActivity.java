9803ed0d4f236cc643f12e51c56fa15e8f9c151e
Lily Lee <lilizaq@hotmail.com>
2016-11-18 23:35:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.ksoap2.serialization.SoapObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String NAMESPACE = "http://tempuri.org/";
+    private static String METHOD_NAME = "HelloWorld";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SoapObject request = new SoapObject()
+    }
+}
