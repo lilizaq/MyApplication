c26e86f8da1a518f81dc09de89e52fc125f658ad
lilizaq <lilizaq@hotmail.com>
2016-11-18 23:38:08 +0800

    Soap Change

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SoapObject request = new SoapObject()
+        SoapObject request = new SoapObject();
+
     }
 }
